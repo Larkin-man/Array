2213cfcbdc0b2025de19ef448ffb10d2f458199f
Larkin <ab88199594ae@mail.ru>
2022-11-09 16:26:39 +0400

    Реализация массива на шаблоне
    v1.0
    - сортировка

--- LarkinArray.h
+++ LarkinArray.h
@@ -1,261 +1,294 @@
-//Динамический массив.
-#ifndef LARKINARRAY
-#define LARKINARRAY
+//Динамический массив. v1.0 При создании элементов конструкторы не вызываются!
+//Опции, задаются командами #define:
+//Для простых типов (int, double etc):
+//#define LRArraySimpleType
+//Автосортировка для простых типов:
+//#define LRArrayAutoSort
+//Создание коллекции (игнорируются добавление дубликата):
+//Define LRArrayCollection
+//---------------------------------------------------------------------------
+#undef LARKINARRAY
+#ifdef LRArrayAutoSort
+   #ifndef LRArraySimpleType
+      #error Version 1.0 does not contain sorting for compound types. Define LRArraySimpleType
+   #endif
+   #ifdef LRArrayCollection
+      #ifndef LarkinCollectionSortH
+         #define LarkinCollectionSortH
+         #define LARKINARRAY LRCollectionSort
+      #endif
+   #else
+      #ifndef LarkinArraySortH
+         #define LarkinArraySortH
+         #define LARKINARRAY LRArraySort
+      #endif
+   #endif
+#else
+   #ifdef LRArrayCollection
+      #ifndef LarkinCollectionH
+         #define LarkinCollectionH
+         #define LARKINARRAY LRCollection
+      #endif
+   #else
+      #ifndef LarkinArrayH
+         #define LarkinArrayH
+         #define LARKINARRAY LRDynArray
+      #endif
+   #endif
+#endif
 
-//Память, которая может увеличиваться в 2 раза
-template< class T >
-class DMemory
-{
-public:
-	// Конструктор
-	DMemory( int initialSize = 0 )
-	{
-		mData = 0;
-		mSize = 0;
-
-		if ( initialSize )
-		{
-			SetSize( initialSize );
-		}
-	}
-
-	// Деструктор
-	~DMemory()
-	{
-		if ( mData )
-		{
-			free( mData );
-		}
-	}
-
-	// Доступ к элементам
-	T& operator[]( int i )
-	{
-		return mData[i];
-	}
-	const T& operator[]( int i ) const
-	{
-		return mData[i];
-	}
-
-	// Базовый указатель
-	T *Base()
-	{
-		return mData;
-	}
-	const T *Base() const
-	{
-		return mData;
-	}
-
-	// Получить текущий размер
-	int Size() const
-	{
-		return mSize;
-	}
-
-	// Убедимся, что памяти хватит для хранения по
-	// крайней мере этого числа элементов.
-	void EnsureCapacity( int capacity )
-	{
-		if ( capacity > mSize )
-		{
-			int newSize = mSize;
-			if ( newSize == 0 )
-				newSize = 1;
-			while ( newSize < capacity )
-				newSize *= 2;
-
-			SetSize( newSize );
-		}
-	}
-
-	// Увеличить размер ровно в два раза
-	void Swell()
-	{
-		int newSize = mSize;
-		if ( newSize == 0 )
-			newSize = 1;
-		newSize *= 2;
-
-		SetSize( newSize );
-	}
-
-private:
-	// Установить новый размер куска памяти
-	void SetSize( int newSize )
-	{
-		if ( mData )
-			mData = (T*)realloc( mData, newSize * sizeof(T) );
-		else
-			mData = (T*)malloc( newSize * sizeof(T) );
-
-		mSize = newSize;
-	}
-
-	// Собственно данные
-	T *mData;
-
-	// Размер выделенной памяти
-	int mSize;
-};
+#ifdef LARKINARRAY
+   #ifndef LRArraySimpleType
+      #include <new>
+   #endif
+#include <alloc.h>
+#include <mem.h>        
+//---------------------------------------------------------------------------
+#define TYPEDf
+#ifdef TYPED
+ #define TEMPLT template< class T >
+ #define T T
+#else
+ #define TEMPLT
+ #define T int
+#endif
 
 // Динамический массив
-template< class T >
-class LARKINARRAY
+TEMPLT class LARKINARRAY
 {
-	enum
+private:
+	T *Data;    //Одномерный массив
+	int FCapacity; //Размер выделенной памяти
+	int FCount; //Число элементов в массиве (может быть меньше размера выделенной памяти)
+   //Защита от копирования
+   LARKINARRAY(const LARKINARRAY&) ;
+   LARKINARRAY&operator=(const LARKINARRAY&) ;
+   #ifdef LRArrayAutoSort
+      #ifdef LRArraySimpleType
+      bool SortingAccordIncrease;
+      #endif
+   #endif
+                          
+protected:
+   //Выделить память
+	void SetCapacity(int newCapacity)
+	{                
+		if ( Data )
+			Data = (T*)realloc( Data, newCapacity * sizeof(T) );
+		else
+			Data = (T*)malloc( newCapacity * sizeof(T) );
+      //Data = new T[newCapacity];
+		FCapacity = newCapacity;
+	}
+
+	//Скопировать часть массива после idx на num : направо+ налево-
+	void MoveElements(int idx, int num)
 	{
-		DEFAULT_RESERVE = 64
-	};
+		int numBytesToCopy = (FCount-idx) * sizeof(T);
+		if (numBytesToCopy > 0)
+			memmove(&Data[idx+num], &Data[idx], numBytesToCopy);
+	}
+         
+   int AddCheck(const T& el)
+   {
+      if (FCount == 0)
+         return 0;
+      int Pos = 0;
+      if (AutoSort)
+      {
+         #ifdef LRArraySimpleType
+         if (SortingAccordIncrease)
+         {
+            for (; Pos < FCount; Pos++)
+               if (Data[Pos] >= el)
+                  break;
+         }
+         else
+         {
+            for (; Pos < FCount; Pos++)
+               if (Data[Pos] <= el)
+                  break;
+         }
+         #endif
+      }
+      if (Duplicates != dupAccept)
+      {
+         
+      }
+      if ( (AutoSort)&&(Duplicates != dupAccept) )
+      {
+         for (int i=0; i < FCount; i++)
+         {
+            if (Data[i] == el)
+               return i;
+         }
+
+      }
+   }
+   //*/
 
 public:
-	// Конструктор
-	LARKINARRAY( int reserveMemory = DEFAULT_RESERVE )
-		: mMemory( reserveMemory )
+   //Конструктор для LRDynArray и LRCollection с одним параметром
+   //Конструктор для LRDynArraySort и LRCollectionSort с двумя параметрами. Сортировка задается в самом начале
+   #ifdef LRArrayAutoSort
+   LARKINARRAY(bool sortingAccordingIncrease, int capacity = 0)
+   {
+      SortingAccordIncrease = sortingAccordingIncrease;
+   #else
+	LARKINARRAY(int capacity = 0)
 	{
-		mCount = 0;
-	}
-	// Деструктор
-	~LARKINARRAY()
-	{
-		Erase();
+   #endif
+		Data = 0;
+      FCount = 0;
+		if (capacity > 0)
+		   SetCapacity(capacity);   
+      else
+         FCapacity = 0;
 	}
 
+   ~LARKINARRAY()
+	{
+      #ifdef LRArraySimpleType
+      FCount = 0;
+      #else
+      Erase();
+      #endif
+      if ( Data )
+		{
+			free( Data );
+         //delete [] Data;
+		}
+
+	}
+   __property int Capacity = { read=FCapacity, write=SetCapacity };
+   __property int Count = { read=FCount };
+   
 	// Доступ к элементам
-	T& operator[]( int i ) { return mMemory[i]; }
-	const T& operator[]( int i ) const { return mMemory[i]; }
-	T& Element( int i ) { return mMemory[i]; }
-	const T& Element( int i ) const { return mMemory[i]; }
-
+	T& operator[]( int i ) { return Data[i]; }
+	const T& operator[]( int i ) const { return Data[i]; }
 	// Базовый указатель
-	T *Base() { return mMemory.Base(); }
-	const T *Base() const { return mMemory.Base(); }
-
+	T *Base() { return Data; }
+	const T *Base() const { return Data; }
 	// Первый и последний элементы
-	T& First() { return mMemory[0]; }
-	T& Last() { return mMemory[mCount-1]; }
-	const T& First() const { return mMemory[0]; }
-	const T& Last() const { return mMemory[mCount-1]; }
+	T& First() { return Data[0]; }
+	T& Last() { return Data[FCount-1]; }
+	const T& First() const { return Data[0]; }
+	const T& Last() const { return Data[FCount-1]; }
+	bool Empty() const { return FCount == 0; } // Пустой?
 
-	// Число элементов в массиве
-	int Count() const { return mCount; }
-
-	// Пустой?
-	bool IsEmpty() const { return Count() == 0; }
-
-	// Добавить элемент в конец массива
-	void PushBack( const T& c )
+   #ifdef LRArrayAutoSort
+   void Add(const T& el)
 	{
-		mMemory.EnsureCapacity( mCount+1 );
-		ConstructCopy( mCount, c );
-		mCount++;
+		EnsureCapacity(FCount+1);
+		ConstructCopy(FCount, el);
+		FCount++;
 	}
-
-	// Удалить последний элемент
-	void PopBack()
+   #else
+	//Добавить элемент в конец массива
+	void PushBack(const T& el)
 	{
-		Destruct( mCount-1 );
-		mCount--;
+		EnsureCapacity(FCount+1);
+		ConstructCopy(FCount, el);
+		FCount++;
 	}
+   #endif
 
-	// Добавить элемент в произвольное место
-	void Insert( int idx, const T& c )
+   void ConstructCopy(int i, const T& el)
 	{
-		mMemory.EnsureCapacity( mCount+1 );
-		MoveElementsRight( idx, 1 );
-		ConstructCopy( idx, c );
-		mCount++;
+      #ifdef LRArraySimpleType
+      Data[i] = el;
+      #else
+      new  (&Data[i]) T( el );  //Конструктор не вызывается
+      #endif
 	}
+	void PopBack() // Удалить последний элемент
+	{
+      #ifndef LRArraySimpleType
+      Destruct(FCount-1);
+      #endif
+		FCount--;
+	}
+   #ifndef LRArraySimpleType
+   void Destruct(int i)
+	{
+		Data[i].~T();
+	}
+   #endif
+   //Добавить элемент в произвольное место
+   #ifndef LRArrayAutoSort
+	void Insert(int idx, const T& el)
+	{
+      if (idx < 0)
+         idx = 0;
+      else if (idx > FCount)
+         idx = FCount;
+		EnsureCapacity(FCount+1);
+		MoveElements(idx, 1);
+      //Data[idx] = el;
+		ConstructCopy(idx, el);
+		FCount++;
+	}
+   #endif
 
 	// Удалить произвольный элемент
-	void Remove( int idx )
+	void Remove(int idx)
 	{
-		Destruct( idx );
-		MoveElementsLeft( idx+1, 1 );
-		mCount--;
+      if (FCount <= 0)
+      {
+         FCount = 0;
+         return;
+      }
+      if (idx < 0)
+         idx = 0;
+      else if (idx > FCount)
+         idx = FCount;
+		Destruct(idx);
+		MoveElements(idx+1, -1);
+		FCount--;
 	}
 
-	// Очистить!
+	//Очистить!
 	void Erase()
 	{
-		while ( mCount > 0 )
+		while (FCount > 0)
 			PopBack();
-	}
+	}            
 
-	// Размер выделенной памяти
-	int MemSize() const
+	//Найти индекс элемента
+	int Find(const T& el) const
 	{
-		return mMemory.Size();
-	}
-
-	// Гарантирует выделение памяти для указанного
-	// числа элементов.
-	void EnsureCapacity( int capacity )
-	{
-		mMemory.EnsureCapacity( capacity );
-	}
-
-	// Увеличивает размер выделенной памяти в два раза
-	void Swell()
-	{
-		mMemory.Swell();
-	}
-
-	// Найти
-	int Find( const T& c, int startIndex = 0 )
-	{
-		for ( int i = startIndex; i < mCount; i++ )
+		for (int i=0; i < FCount; i++)
 		{
-			if ( mMemory[i] == c )
+			if (Data[i] == el)
 				return i;
 		}
 		return -1;
-	}
+	}  
 
-	// Найти и уничтожить! >)
-	void RemoveElement( const T& c )
+	//Найти и уничтожить!
+	void RemoveElement(const T& el)
 	{
-		int i = Find( c );
-		if ( i >= 0 )
-			Remove( i );
-	}
+		int idx = Find( el );
+		if (idx >= 0)
+			Remove( idx );
+	}         
 
-private:
-	void ConstructCopy( int i, const T& c )
+	//Убедимся, что памяти хватит для хранения по крайней мере этого числа элементов.
+	inline void EnsureCapacity(int capacity)
 	{
-		new ( &mMemory[i] ) T( c );
-	}
-	void Destruct( int i )
-	{
-		mMemory[i].~T();
-	}
-
-	// Подвинуть хвост направо
-	void MoveElementsRight( int idx, int num )
-	{
-		int numBytesToCopy = (mCount-idx) * sizeof(T);
-		if ( numBytesToCopy > 0 )
+		if ( capacity > FCapacity )
 		{
-			memmove( &mMemory[idx+num], &mMemory[idx], numBytesToCopy );
+			int newCapacity = FCapacity;
+			if ( newCapacity == 0 )
+				newCapacity = 4;
+			while ( newCapacity < capacity )
+				newCapacity *= 2;    
+			SetCapacity( newCapacity );
 		}
 	}
 
-	// Подвинуть хвост налево
-	void MoveElementsLeft( int idx, int num )
-	{
-		int numBytesToCopy = (mCount-idx) * sizeof(T);
-		if ( numBytesToCopy > 0 )
-		{
-			memmove( &mMemory[idx-num], &mMemory[idx], numBytesToCopy );
-		}
-	}
-
-	// Память, где мы храним данные
-	DMemory<T> mMemory;
-
-	// Число элементов в массиве (может быть меньше размера выделенной памяти!)
-	int mCount;
 };
-
 #endif
+#undef LRArraySimpleType
+#undef LRArrayAutoSort
+#undef LRArrayCollection
